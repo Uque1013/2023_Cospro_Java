c29c0c4a397fcdf70960e8686d30b95f1a3c42cf
kye <kye6735@naver.com>
2023-05-10 17:09:19 +0900

    3차 2급 7번 사과3+당근1 = 주스 1잔, 토끼에게 먹일거는 k개, 주스는 최대한 많이 만들기

---
+++ 2023_Cospro_Java/src/Test_230510_03/Test_07.java
@@ -0,0 +1,44 @@
+package Test_230510_03;
+
+class Solution {
+	public int solution(int numApple, int numCarrot, int k) {
+		int answer = 0;
+		if(numApple < 3 * numCarrot)
+			answer = numApple / 3;
+		else 
+			answer = numCarrot;
+		
+		numApple -= 3 * answer;
+		numCarrot -= answer;
+		
+		for(int i = 0; k - (numApple + numCarrot + i) > 0; i++)
+			if(i % 4 == 0)
+				answer --;
+		
+		return answer;
+	}
+}
+
+public class Test_07 {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+        int numApple1 = 5;
+        int numCarrot1 = 1;
+        int k1 = 2;
+        int ret1 = sol.solution(numApple1, numCarrot1, k1);
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+        
+        int numApple2 = 10;
+        int numCarrot2 = 5;
+        int k2 = 4;
+        int ret2 = sol.solution(numApple2, numCarrot2, k2);
+        
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+
+	}
+
+}
